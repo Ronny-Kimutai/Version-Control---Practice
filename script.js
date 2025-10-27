65483b1088eeaf704992f9de6167fdddb6c65a11
Kimutai <ronnykibiwott505@gmail.com>
2025-10-27 16:04:10 +0300

    Logout

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // script.js
 
 // A simple JavaScript program that logs a message to the console
+// This is for the Logout screen.....YEAH!
 function greet(name) {
     console.log(`Hello, ${name}! Welcome to the world of JavaScript.`);
 }
