056d9a929428567ee74e0508d33c1d26d27c39f6
Kimutai <ronnykibiwott505@gmail.com>
2025-10-27 15:37:19 +0300

    JS file

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+// A simple JavaScript program that logs a message to the console
+function greet(name) {
+    console.log(`Hello, ${name}! Welcome to the world of JavaScript.`);
+}
+
+// Call the greet function
+greet('Ronny');
